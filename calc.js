0cd51a3a644852533dea20ef2d8f624292dc6df5
soniagkalliath <soniageorge111@gmail.com>
2022-06-29 10:44:28 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,20 @@
+//1. display numbers in text box
+function displayNum(num){
+    result.value +=num
+}
+
+//2. clear content in text box
+function clearBox(){
+    result.value=""
+}
+
+//3. evaluate expression
+function evaluateExpr(){
+    result.value=eval(result.value)
+}
+
+//4. remove last item from text box
+function removeItem(){
+    curExpr=result.value
+    result.value=curExpr.slice(0,-1)
+}
